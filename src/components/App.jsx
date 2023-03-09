dcd74c5e5c20490ad16aed7118f19ac862b990d7
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-03-09 20:35:57 +0200

    statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from 'components/profile/Profile';
 import user from 'dataBase/user.json';
+import { Statistics } from 'components/statistics/Statistics';
+import data from 'dataBase/data.json'
 
 export const App = () => {
   return (
@@ -10,7 +12,8 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
-      </>
+      />      
+      <Statistics stats={data} />
+    </>
   );
 };
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,3 +1,42 @@
-<div>
-    
-</div>
+import PropTypes from 'prop-types';
+import { getRandomHexColor } from 'js/RandomHexColor';
+import css from 'components/statistics/Statistics.module.css';
+
+// створюємо компонент реакт
+// якщо title  не переданий, не повинна рендеритись розмітка заголовка <h2>
+// у розмітку підгружається масив з файлу data.json
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {/* {title && <h2 className={css.title}>{title}</h2>} */}
+      <h2 className={css.title}> Upload stats</h2>
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              key={id}
+              className={css.item}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
